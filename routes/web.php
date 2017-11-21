7ae5a674e763769d9372dbad72e91f5ca5b1c8ce
Cy Rossignol <cy@rossignols.me>
2017-11-21 23:13:40 +0000

    Scaffold auth controllers and routes
    Signed-off-by: Cy Rossignol <cy@rossignols.me>

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('home');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
