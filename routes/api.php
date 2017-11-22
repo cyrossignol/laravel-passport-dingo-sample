d9d114d6626c7b332270d43e6ea64357e92931f5
Cy Rossignol <cy@rossignols.me>
2017-11-22 01:47:33 +0000

    Create example API endpoints for testing
    Signed-off-by: Cy Rossignol <cy@rossignols.me>

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,65 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+
+    // Copy and paste the 'axios' statements into the browser's JavaScript
+    // console to make API calls.
+
+    // This endpoint should successfully respond when logged out.
+    //
+    // Note that this endpoint doesn't account for the CSRF token state. If you
+    // send this while logged in and get stuck, call "axios.post('logout')" and
+    // log back in.
+    //
+    // axios.get('/api/guest').then(response => console.log(response));
+    $api->get('guest', function (Request $request) {
+        return [
+            'endpoint' => 'guest',
+            'user' => $request->user(),
+        ];
+    });
+
+    // These endpoints should respond with 'unauthenticated' until the user
+    // logs in. Then, we should see information about the authenticated user.
+    $api->group([ 'middleware' => 'auth:api-combined' ], function ($api) {
+
+        // axios.get('/api/get').then(response => console.log(response));
+        $api->get('get', function (Request $request) {
+            return [
+                'endpoint' => 'get',
+                'user' => $request->user(),
+            ];
+        });
+
+        // axios.post('/api/post').then(response => console.log(response));
+        $api->post('post', function (Request $request) {
+            return [
+                'endpoint' => 'post',
+                'user' => $request->user(),
+            ];
+        });
+
+        // axios.get('/api/internal-get').then(response => console.log(response));
+        $api->get('internal-get', function (Request $request) {
+            return [
+                'endpoint' => 'internal-get',
+                'user' => $request->user(),
+                'internal' => app('Dingo\Api\Dispatcher')->get('get'),
+            ];
+        });
+
+        // axios.post('/api/internal-post').then(response => console.log(response));
+        $api->post('internal-post', function (Request $request) {
+            return [
+                'endpoint' => 'internal-post',
+                'user' => $request->user(),
+                'internal' => app('Dingo\Api\Dispatcher')->post('post'),
+            ];
+        });
+
+    });
+
 });
